542f202d19e4620f5afeae50acafb304dd2af98c
Grzegorz <grzesiu1997@gmail.com>
2021-06-03 21:35:53 +0200

    Remote state in Azure and bash script for creating storage account in Azure

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name   = "Terraform"
+    storage_account_name  = "tfstate31973"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
@@ -6,11 +12,11 @@ terraform {
     }
   }
   required_version = ">= 0.14.9"
-  }
+}
 provider "azurerm" {
-    features {}
-  }
-  resource "azurerm_resource_group" "rg" {
-    name = "myTFResourceGroup"
-    location = "northeurope"
-  }
+  features {}
+}
+resource "azurerm_resource_group" "Terraform_resource_group" {
+  name = "Terraform"
+  location = "northeurope"
+}
