29149c03a2e7292058ea8b61379abb5cd660b4df
Grzegorz <grzesiu1997@gmail.com>
2021-06-12 21:01:02 +0200

    Added missing RDP access rule

--- NSGs.tf
+++ NSGs.tf
@@ -4,6 +4,18 @@ resource "azurerm_network_security_group" "NSG-main" {
   resource_group_name = azurerm_resource_group.Terraform_resource_group.name
 
   security_rule {
+    name = "RDP"
+    priority = 1000
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "3389"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
     name = "SSH"
     priority = 1001
     direction = "Inbound"
